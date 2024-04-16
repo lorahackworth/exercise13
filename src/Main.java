5cb6ac8375962bb266b909a8a31608ed1cc35af1
lorahackworth <lora.g.hackworth@gmail.com>
2024-04-16 13:55:32 -0400

    s

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+class CheckingAccount {
+    private int balance;
+    public int getBalance() {
+        return balance;
+    }
+    public CheckingAccount(int initialBalance)
+    {
+        balance = initialBalance;
+    }
+    synchronized public int withdraw(int amount)
+    {
+        if (amount <= balance)
+        {
+            try {
+                Thread.sleep((int) (Math.random() * 200));
+            }
+            catch (InterruptedException ie) {
+            }
+            balance -= amount;
+        }
+        return balance;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CheckingAccount account = new CheckingAccount(100);
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                String name = Thread.currentThread().getName();
+                for (int i = 0; i < 10; i++) {
+                    if(!(account.getBalance() == 0)){
+                        System.out.println(name + " tries to withdraw $10, balance: " + account.withdraw(10));
+                    }
+                }
+            }
+        };
+
+        Thread thdHusband = new Thread(r);
+        thdHusband.setName("Husband");
+
+        Thread thdWife = new Thread(r);
+        thdWife.setName("Wife");
+
+        thdHusband.start();
+        thdWife.start();
     }
 }
